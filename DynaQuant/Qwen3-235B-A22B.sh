7674cffafae670193991b33146d0d15f3ab1d882
kexinchu <chukexin2017@gmail.com>
2025-08-22 01:27:36 +0000

    update DynaQuant

---
+++ DynaQuant/Qwen3-235B-A22B.sh
@@ -0,0 +1,20 @@
+#/bin/sh
+
+CUDA_VISIBLE_DEVICES=0,1,2,3 \
+python3 -m sglang.launch_server \
+  --model-path /dcar-vepfs-trans-models/Qwen3-235B-A22B \
+  --tp-size 1 \
+  --dp-size 4 \
+  --enable-ep-moe \
+  --max-running-requests 32 \
+  --host 127.0.0.1 --port 8080 \
+  --max-total-tokens 40960 \
+  --dtype bfloat16 \
+  --trust-remote-code \
+  --attention-backend torch_native \
+  --sampling-backend pytorch \
+  --disable-cuda-graph \
+  --disable-cuda-graph-padding \
+  --kv-cache-dtype auto \
+  --allow-auto-truncate \
+  --chunked-prefill-size 16384 \
